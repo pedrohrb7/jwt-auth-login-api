a8fa9182298d25d21ceb49b6e833d0c1beb04c33
Pedro <phrbrock@gmail.com>
2022-06-29 14:59:41 -0300

    feat(user): implements create

---
+++ src/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.user.create({
+      data: user,
+    });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+}
