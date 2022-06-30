ccd83f5e2537fd0de2c03a0803fdc12eea976428
Pedro <phrbrock@gmail.com>
2022-06-30 10:50:08 -0300

    feat(auth): implement jwt token to login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.EXP_JWT,
+      },
+    }),
+  ],
 })
 export class AuthModule {}
